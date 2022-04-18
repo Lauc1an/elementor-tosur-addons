32bca8a376f9cc6b9fe3c66bfbc1c1e6062043ee
Lauc1an <marlomp@hotmail.com>
2022-04-18 01:45:58 -0500

    Building basic structure of Elementor Widget

---
+++ elementor-tosur-addons.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Elementor Tosur Forms Addons
+ *
+ * @package ElementorTosurAddons
+ *
+ * Plugin Name: Elementor Tosur Forms Addons
+ * Description: Customs forms to select products in Tosur
+ * Plugin URI:  https://github.com/Lauc1an/elementor-tosur-addons
+ * Version:     1.0.0
+ * Author:      Laucian
+ * Author URI:  https://github.com/Lauc1an
+ * Text Domain: elementor-tosur-addons
+ */
+
+define( 'ELEMENTOR_TOSUR_ADDONS', __FILE__ );
+
+/**
+ * Include the Elementor_Tosur_Addons class.
+ */
+require plugin_dir_path( ELEMENTOR_TOSUR_ADDONS ) . 'class-elementor-tosur-addons.php';
---
+++ widgets/class-tosur-brevete.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Tosur Brevete class.
+ *
+ * @category   Class
+ * @package    ElementorTosurAddons
+ * @subpackage WordPress
+ * @author     Laucian <alexander@aerastudio.pe>
+ * @copyright  2022 Laucian
+ * @license    https://opensource.org/licenses/GPL-3.0 GPL-3.0-only
+ * @link       link(https://github.com/Lauc1an/elementor-tosur-addons,
+ *             Elementor Widgets for Tosur Product Forms)
+ * @since      1.0.0
+ * php version 7.4.26
+ */
+
+namespace ElementorTosurAddons\Widgets;
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+// Security Note: Blocks direct access to the plugin PHP files.
+defined( 'ABSPATH' ) || die();
+
+/**
+ * TosurBrevete widget class.
+ *
+ * @since 1.0.0
+ */
+class TosurBrevete extends Widget_Base {
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $data Widget data.
+	 * @param array $args Widget arguments.
+	 */
+	public function __construct( $data = array(), $args = null ) {
+		parent::__construct( $data, $args );
+		wp_register_style( 'tosur-brevete', plugins_url( '/assets/css/tosur-brevete.css', ELEMENTOR_TOSUR_ADDONS ), array(), '1.0.0' );
+	}
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'tosur-brevete';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Tosur Brevete', 'elementor-tosur-addons' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-form-horizontal';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return array( 'general' );
+	}
+	
+	/**
+	 * Enqueue styles.
+	 */
+	public function get_style_depends() {
+		return array( 'tosur-brevete' );
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			array(
+				'label' => __( 'Contenido', 'elementor-tosur-addons' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			)
+		);
+
+		$this->add_control(
+			'form_name',
+			array(
+				'label'   => __( 'Form Name', 'elementor-tosur-addons' ),
+				'type'    => Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'placeholder' => __( 'Form Name', 'elementor-tosur-addons' ),
+			)
+		);
+
+		$this->end_controls_section();
+
+	}
+    
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+		$name = $settings['form_name'];
+		?>
+		<form name="<?= $name; ?>" method="POST" action="">
+			<label for="<?= $name; ?>_name">Nombre</label>
+			<input id="<?= $name; ?>_name" type="text">
+			<button type="submit">Enviar</button>
+		</form>
+		<?php
+	}
+
+	protected function content_template() {
+		?>
+		<form name="{{ settings.title }}" method="POST" action="">
+			<label for="{{ settings.title }}_name">Nombre</label>
+			<input id="{{ settings.title }}_name" type="text">
+			<button type="submit">Enviar</button>
+		</form>
+		<?php
+	}
+	
+}
